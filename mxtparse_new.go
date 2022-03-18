2f0ea724602ab9733f778f0c900d8e6cbbee85e0
N. Minaev <minaev.36.ru@gmail.com>
2022-03-19 02:56:28 +0300

    trouble with goroutines

--- mxtparse_new.go
+++ mxtparse_new.go
@@ -10,13 +10,17 @@ package main
 */
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"os"
 	"runtime"
 	"strconv"
+	"sync"
 	"text/template"
 
 	"github.com/xuri/excelize/v2"
+	"golang.org/x/text/encoding/charmap"
 )
 
 // flag variables initialization, pointer to *type of flag
@@ -25,49 +29,41 @@ var (
 	OutputFile    *string
 	FullFile      *bool
 	DisplayOutput *bool
+	// syncronize goroutines
+	WorkGroup sync.WaitGroup
 )
 
-// flag declarations according to types of variables
-func init() {
-	InputFile = flag.String("InputFile", "apt.xlsx", "Filepath to input XLSX file")
-	OutputFile = flag.String("OutputFile", "apt.mxtsessions", "Filepath to output .mxt file")
-	FullFile = flag.Bool("FullFile", true, "Add cashiers PC's to file?")
-	DisplayOutput = flag.Bool("ShowOutput", false, "Output parsed connections to terminal?")
-}
-func main() {
-	flag.Parse()      // get flags
-	PrintParameters() // show parameters of task
-	XLSX := OpenXLSXFile(*InputFile)
-	XLSXChannelOut := make(chan RDP)             // channel to XLSX out
-	ReadXLSXFile(XLSX, "Аптеки", XLSXChannelOut) // read from file to channel
-	if !*FullFile {
-		// declare template
-		MXTTemplate := template.New("MobaXTermTemplate")
-		MXTTemplateText := "\n\n[Bookmarks_{{.RecordNum}}]\nSubRep={{.RKName}}\\{{.AptName}}\nImgNum=41\n{{.AptName}}({{.APCode}})=#91#4%{{.ServerAddress}}.apt.rigla.ru%10433%[{{.Username}}]%0%-1%-1%-1%-1%0%0%-1%%%%%0%0%%-1%%-1%-1%0%-1%0%-1#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%-1%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1#0# #-1"
-		MXTTemplate.Parse(MXTTemplateText)
-		for i := 0; i <= runtime.NumCPU(); i++ { //goroutine count will be equal CPU core count
-			//	go ParseTemplate()
-		}
-	} else {
-		for i := 0; i <= runtime.NumCPU(); i++ {
-
-		}
-	}
-}
-
-// read struct from *RDP channel to text/template lib, result to chan []byte to WriteFile()
+// read struct from *RDP channel to text/template lib, result -> chan []byte -> WriteFile()
 func ParseTemplate(TemplateText *template.Template, IncomeRDPChannel chan RDP, ChannelOut chan []byte) {
+	defer func() { // finish goroutines
+		WorkGroup.Done()
+		println("Parsing connections done!")
+	}()
+	// []byte to get template execute results
+	var ParsedString bytes.Buffer
 	// get *RDP from chan
 	RDParsed := <-IncomeRDPChannel
-	//TemplateText.Execute(
+	TemplateText.Execute(&ParsedString, &RDParsed)
+	ChannelOut <- ParsedString.Bytes()
 }
 
-func WriteFile() {
-
+func WriteToFile(FileName *os.File, EncodedChannel chan []byte) {
+	defer func() {
+		WorkGroup.Done()
+		println("Writing connections to file done!")
+	}()
+	ParsedString := string(<-EncodedChannel) //[]byte to str
+	//create encoder
+	IBM855Encode := charmap.CodePage855.NewEncoder()
+	ConnectionString, _ := IBM855Encode.String(ParsedString)
+	FileName.Write([]byte(ConnectionString))
 }
 
 // read file line by line from XLSX list, creating RDP struct and take it to free goroutine
 func ReadXLSXFile(XLSXFile *excelize.File, ListName string, ChannelOut chan RDP) {
+	defer func() {
+		WorkGroup.Done()
+	}()
 	Rows, err := XLSXFile.GetRows(ListName)
 	if err != nil {
 		println(err)
@@ -117,4 +113,47 @@ func OpenXLSXFile(Filepath string) *excelize.File {
 		}
 	}()
 	return File
+}
+
+// flag declarations according to types of variables
+func init() {
+	InputFile = flag.String("InputFile", "apt.xlsx", "Filepath to input XLSX file")
+	OutputFile = flag.String("OutputFile", "apt.mxtsessions", "Filepath to output .mxt file")
+	FullFile = flag.Bool("FullFile", true, "Add cashiers PC's to file?")
+	DisplayOutput = flag.Bool("ShowOutput", false, "Output parsed connections to terminal?")
+}
+func main() {
+	// two types of goroutine will be use
+	WorkGroup.Add(8)
+	flag.Parse()      // get flags
+	PrintParameters() // show parameters of task
+	XLSX := OpenXLSXFile(*InputFile)
+	XLSXChannelOut := make(chan RDP)      // channel to XLSX output
+	ParsedChannelOut := make(chan []byte) // channel to template.Parse output
+	if *FullFile {
+		// declare template
+		MXTTemplate := template.New("MobaXTermTemplate")
+		MXTTemplateText := "\n\n[Bookmarks_{{.RecordNum}}]\nSubRep={{.RKName}}\\{{.AptName}}\nImgNum=41\n{{.AptName}}({{.APCode}})=#91#4%{{.ServerAddress}}.apt.rigla.ru%10433%[{{.Username}}]%0%-1%-1%-1%-1%0%0%-1%%%%%0%0%%-1%%-1%-1%0%-1%0%-1#MobaFont%10%0%0%-1%15%236,236,236%30,30,30%180,180,192%0%-1%0%%xterm%-1%-1%_Std_Colors_0_%80%24%0%1%-1%<none>%%0%0%-1#0# #-1"
+		MXTTemplate.Parse(MXTTemplateText)
+		//create output file
+		OutFile, err := os.OpenFile(*OutputFile, os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+		defer func() {
+			OutFile.Close()
+		}()
+		ReadXLSXFile(XLSX, "Аптеки", XLSXChannelOut) // read from file to channel
+		//end of output file description
+		for i := 0; i <= 3; i++ { //goroutine count will be equal CPU core count
+			go ParseTemplate(MXTTemplate, XLSXChannelOut, ParsedChannelOut)
+			go WriteToFile(OutFile, ParsedChannelOut)
+		}
+	} else {
+		for i := 0; i <= runtime.NumCPU(); i++ {
+
+		}
+	}
+	WorkGroup.Wait()
+	println("Job done!")
 }
